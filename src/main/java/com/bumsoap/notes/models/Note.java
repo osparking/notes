8e3d2339e580779825687cd723ab3c93e07ed3e8
박종범 <jbpark03@gmail.com>
2025-04-23 11:30:07 +0900

    Note model/entity class

--- src/main/java/com/bumsoap/notes/models/Note.java
+++ src/main/java/com/bumsoap/notes/models/Note.java
@@ -1,4 +1,17 @@
 package com.bumsoap.notes.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String content;
+
+    private String ownerUsername;
 }
