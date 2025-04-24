29d1053acc5b43202ea67c2afee4e8e3adbaf240
박종범 <jbpark03@gmail.com>
2025-04-24 09:23:14 +0900

    in memory user details service bean

--- src/main/java/com/bumsoap/notes/security/SecurityConfig.java
+++ src/main/java/com/bumsoap/notes/security/SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,15 +19,30 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/contact").permitAll()
-                .requestMatchers("/public/**").permitAll()
-                .requestMatchers("/admin").denyAll()
                 .anyRequest().authenticated());
-//        http.formLogin(withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
-        http.sessionManagement(session -> session.sessionCreationPolicy(
-                SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        if (!manager.userExists("user1")) {
+            manager.createUser(User
+                    .withUsername("user1")
+                    .password("{noop}1234")
+                    .roles("USER")
+                    .build());
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(User
+                    .withUsername("admin")
+                    .password("{noop}1234")
+                    .roles("ADMIN")
+                    .build());
+        }
+        return manager;
     }
 }
