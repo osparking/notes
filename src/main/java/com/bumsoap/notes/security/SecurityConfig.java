3b75c4b8989b23de86d2de04ca5d35fd46f135db
박종범 <jbpark03@gmail.com>
2025-04-24 16:14:34 +0900

    adopt UserDetailsManager class of Jdbc

--- src/main/java/com/bumsoap/notes/security/SecurityConfig.java
+++ src/main/java/com/bumsoap/notes/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -27,7 +28,7 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        var manager = new JdbcUserDetailsManager();
 
         if (!manager.userExists("user1")) {
             manager.createUser(User
