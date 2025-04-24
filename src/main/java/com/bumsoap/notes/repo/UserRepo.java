7b72aa8d90210a9f253fe17d09ad373099cdb955
박종범 <jbpark03@gmail.com>
2025-04-25 07:44:19 +0900

    user repository interface w/ findByUsername

---
+++ src/main/java/com/bumsoap/notes/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.bumsoap.notes.repo;
+
+import com.bumsoap.notes.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
