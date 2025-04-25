7295aafbad7025470e3e6667d338684cf5709fac
박종범 <jbpark03@gmail.com>
2025-04-25 14:43:28 +0900

    role repository - findByName

---
+++ src/main/java/com/bumsoap/notes/repo/RoleRepo.java
@@ -0,0 +1,11 @@
+package com.bumsoap.notes.repo;
+
+import com.bumsoap.notes.models.AppRole;
+import com.bumsoap.notes.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(AppRole appRole);
+}
