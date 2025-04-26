2ed1bb34200b06892de8f262302b2a6ce4189b8f
박종범 <jbpark03@gmail.com>
2025-04-26 20:41:21 +0900

    admin controller of 3 end points

---
+++ src/main/java/com/bumsoap/notes/cont/AdminController.java
@@ -0,0 +1,38 @@
+package com.bumsoap.notes.cont;
+
+import com.bumsoap.notes.dtos.UserDto;
+import com.bumsoap.notes.models.User;
+import com.bumsoap.notes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id),
+                HttpStatus.OK);
+    }
+
+    @PutMapping("/update-role")
+    public ResponseEntity<String> updateUserRole(
+            @RequestParam Long userId, @RequestParam String roleName) {
+        userService.updateUserRole(userId, roleName);
+        return ResponseEntity.ok("유저 롤 갱신됨");
+    }
+
+}
