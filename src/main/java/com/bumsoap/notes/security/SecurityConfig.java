2f4624d7113066c4af914f0b837277ef357a97cf
박종범 <jbpark03@gmail.com>
2025-04-27 08:08:03 +0900

    enables method level security annotations

--- src/main/java/com/bumsoap/notes/security/SecurityConfig.java
+++ src/main/java/com/bumsoap/notes/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bumsoap.notes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,10 +18,15 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
         http.authorizeHttpRequests((requests) -> requests
                 .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
