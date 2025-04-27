42757a84b0afc98d6ea54a1b6a890c558cc89e46
박종범 <jbpark03@gmail.com>
2025-04-27 11:44:48 +0900

    find login user id and pass it to service

--- src/main/java/com/bumsoap/notes/cont/AdminController.java
+++ src/main/java/com/bumsoap/notes/cont/AdminController.java
@@ -2,41 +2,44 @@ package com.bumsoap.notes.cont;
 
 import com.bumsoap.notes.dtos.UserDto;
 import com.bumsoap.notes.models.User;
+import com.bumsoap.notes.security.serv.UserDetailsImpl;
 import com.bumsoap.notes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
-//@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
     @Autowired
     UserService userService;
 
     @GetMapping("/getusers")
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(),
                 HttpStatus.OK);
     }
 
     @GetMapping("/user/{id}")
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
-        return new ResponseEntity<>(userService.getUserById(id),
+        Long loginId = ((UserDetailsImpl) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal()).getId();
+
+        return new ResponseEntity<>(userService.getUserById(loginId, id),
                 HttpStatus.OK);
     }
 
     @PutMapping("/update-role")
-    public ResponseEntity<String> updateUserRole(
+    public ResponseEntity<UserDto> updateUserRole(
             @RequestParam Long userId, @RequestParam String roleName) {
-        userService.updateUserRole(userId, roleName);
-        return ResponseEntity.ok("유저 롤 갱신됨");
+        UserDto userDto = userService.updateUserRole(userId, roleName);
+        return ResponseEntity.status(HttpStatus.OK).body(userDto);
     }
 
 }
