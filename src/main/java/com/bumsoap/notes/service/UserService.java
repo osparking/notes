5ebdd2665e22180078b8d80036251652063774f3
박종범 <jbpark03@gmail.com>
2025-04-27 11:34:43 +0900

    allow admin at user service interface

--- src/main/java/com/bumsoap/notes/service/UserService.java
+++ src/main/java/com/bumsoap/notes/service/UserService.java
@@ -2,11 +2,17 @@ package com.bumsoap.notes.service;
 
 import com.bumsoap.notes.dtos.UserDto;
 import com.bumsoap.notes.models.User;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface UserService {
-    void updateUserRole(Long userId, String roleName);
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    UserDto updateUserRole(Long userId, String roleName);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     List<User> getAllUsers();
-    UserDto getUserById(Long id);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR #loginId == #userId")
+    UserDto getUserById(Long loginId, Long userId);
 }
