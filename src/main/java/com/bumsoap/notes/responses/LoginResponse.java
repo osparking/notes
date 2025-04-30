7765f991986bea21504d2cfd4296134d5fd30739
박종범 <jbpark03@gmail.com>
2025-04-30 10:15:03 +0900

    reduce code by using lombok data anno'

--- src/main/java/com/bumsoap/notes/responses/LoginResponse.java
+++ src/main/java/com/bumsoap/notes/responses/LoginResponse.java
@@ -1,7 +1,12 @@
 package com.bumsoap.notes.responses;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
 public class LoginResponse {
   private String jwtToken;
   private String username;
@@ -13,27 +18,4 @@ public class LoginResponse {
     this.jwtToken = jwtToken;
   }
 
-  public String getJwtToken() {
-    return jwtToken;
-  }
-
-  public void setJwtToken(String jwtToken) {
-    this.jwtToken = jwtToken;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public List<String> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(List<String> roles) {
-    this.roles = roles;
-  }
 }
