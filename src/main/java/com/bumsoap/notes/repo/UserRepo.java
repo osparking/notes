3fa90c298f60dfec1a1fddedde1b43935947c59e
박종범 <jbpark03@gmail.com>
2025-05-01 08:49:10 +0900

    finds user by her username

--- src/main/java/com/bumsoap/notes/repo/UserRepo.java
+++ src/main/java/com/bumsoap/notes/repo/UserRepo.java
@@ -8,8 +8,11 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+  Optional<User> findByUsername(String username);
 
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+
+  Optional<User> findByUserName(String username);
 }
