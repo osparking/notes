6dc29f5947d0ef768361ad76bb9869814046511d
박종범 <jbpark03@gmail.com>
2025-04-30 19:20:41 +0900

    user sign up end point method

--- src/main/java/com/bumsoap/notes/cont/AuthCon.java
+++ src/main/java/com/bumsoap/notes/cont/AuthCon.java
@@ -1,13 +1,23 @@
 package com.bumsoap.notes.cont;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.bumsoap.notes.models.AppRole;
+import com.bumsoap.notes.models.Role;
+import com.bumsoap.notes.models.User;
+import com.bumsoap.notes.repo.RoleRepo;
+import com.bumsoap.notes.repo.UserRepo;
 import com.bumsoap.notes.requests.LoginRequest;
+import com.bumsoap.notes.requests.SignUpRequest;
 import com.bumsoap.notes.responses.LoginResponse;
+import com.bumsoap.notes.responses.MessageResponse;
 import com.bumsoap.notes.security.jwt.JwtUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +27,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +39,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthCon {
   private final AuthenticationManager authenticationManager;
   private final JwtUtils jwtUtils;
+  private final UserRepo userRepo;
+  private final PasswordEncoder encoder;
+  private final RoleRepo roleRepo;
 
   @PostMapping("/public/signin")
   public ResponseEntity<?> authenticateUser(
@@ -59,9 +72,63 @@ public class AuthCon {
         .collect(Collectors.toList());
 
     // Prepare the response body, now including the JWT token directly in the body
-    LoginResponse response = new LoginResponse(userDetails.getUsername(), roles, jwtToken);
+    LoginResponse response =
+        new LoginResponse(userDetails.getUsername(), roles, jwtToken);
 
     // Return the response entity with the JWT token included in the response body
     return ResponseEntity.ok(response);
+  }
+
+  @PostMapping("/public/signup")
+  public ResponseEntity<?> registerUser(
+      @Valid @RequestBody SignUpRequest signUpRequest) {
+    if (userRepo.existsByUsername(signUpRequest.getUsername())) {
+      return ResponseEntity.badRequest().body(
+          new MessageResponse("Error: Username is already taken!"));
+    }
+
+    if (userRepo.existsByEmail(signUpRequest.getEmail())) {
+      return ResponseEntity.badRequest().body(
+          new MessageResponse("Error: Email is already in use!"));
+    }
+
+    // Create new user's account
+    User user = new User(signUpRequest.getUsername(),
+        signUpRequest.getEmail(),
+        encoder.encode(signUpRequest.getPassword()));
+
+    Set<String> strRoles = signUpRequest.getRole();
+    Role role;
+
+    if (strRoles == null || strRoles.isEmpty()) {
+      role = roleRepo.findByRoleName(AppRole.ROLE_USER)
+          .orElseThrow(() ->
+              new RuntimeException("Error: Role is not found."));
+    } else {
+      String roleStr = strRoles.iterator().next();
+      if (roleStr.equals("admin")) {
+        role = roleRepo.findByRoleName(AppRole.ROLE_ADMIN)
+            .orElseThrow(() ->
+                new RuntimeException("Error: Role is not found."));
+      } else {
+        role = roleRepo.findByRoleName(AppRole.ROLE_USER)
+            .orElseThrow(() ->
+                new RuntimeException("Error: Role is not found."));
+      }
+
+      user.setAccountNonLocked(true);
+      user.setAccountNonExpired(true);
+      user.setCredentialsNonExpired(true);
+      user.setEnabled(true);
+      user.setCredentialsExpiration(LocalDate.now().plusYears(1));
+      user.setAccountExpiration(LocalDate.now().plusYears(1));
+      user.setTwoFactorEnabled(false);
+      user.setSignUpMethod("email");
+    }
+    user.setRole(role);
+    userRepo.save(user);
+
+    return ResponseEntity.ok(
+        new MessageResponse("User registered successfully!"));
   }
 }
