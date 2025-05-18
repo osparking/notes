f26268e76d10ab7440672ceb89d55aebb92de147
박종범 <jbpark03@gmail.com>
2025-05-18 15:29:37 +0900

    find note audio logs by note ID

--- src/main/java/com/bumsoap/notes/repo/NoteAuditRepo.java
+++ src/main/java/com/bumsoap/notes/repo/NoteAuditRepo.java
@@ -3,6 +3,9 @@ package com.bumsoap.notes.repo;
 import com.bumsoap.notes.models.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteAuditRepo
     extends JpaRepository<AuditLog, Long> {
+  List<AuditLog> findByNoteId(Long noteId);
 }
