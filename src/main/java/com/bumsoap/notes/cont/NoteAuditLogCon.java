ef57063c786e47653fdc7d5b46aaa1e78cbf0a16
박종범 <jbpark03@gmail.com>
2025-05-18 14:57:51 +0900

    audit log for note fetch controller

---
+++ src/main/java/com/bumsoap/notes/cont/NoteAuditLogCon.java
@@ -0,0 +1,27 @@
+package com.bumsoap.notes.cont;
+
+import com.bumsoap.notes.models.AuditLog;
+import com.bumsoap.notes.service.NoteAudit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/audit")
+public class NoteAuditLogCon {
+  @Autowired
+  private NoteAudit noteAudit;
+
+  @GetMapping
+  public List<AuditLog> getAllAuditLogs() {
+
+  }
+
+  @GetMapping("/note/{id")
+  public List<AuditLog> getAuditLogsFor(Long noteId) {
+
+  }
+}
