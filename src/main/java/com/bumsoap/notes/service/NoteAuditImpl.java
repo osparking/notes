9aac491f0772bb5f7696d2a011d57c725594d4f1
박종범 <jbpark03@gmail.com>
2025-05-18 11:08:57 +0900

    Note audit service class - imple

---
+++ src/main/java/com/bumsoap/notes/service/NoteAuditImpl.java
@@ -0,0 +1,16 @@
+package com.bumsoap.notes.service;
+
+import com.bumsoap.notes.models.Note;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteAuditImpl {
+  public void logCreate(String username, Note note) {
+  }
+
+  public void logUpdate(String username, Note note) {
+  }
+
+  public void logDelete(String username, Note note) {
+  }
+}
