adc86568a590bf6c3b6110f67e105d27db8d4d43
박종범 <jbpark03@gmail.com>
2025-05-18 11:29:36 +0900

    create note creation log record

--- src/main/java/com/bumsoap/notes/service/impl/NoteAuditImpl.java
+++ src/main/java/com/bumsoap/notes/service/impl/NoteAuditImpl.java
@@ -1,13 +1,23 @@
 package com.bumsoap.notes.service.impl;
 
+import com.bumsoap.notes.models.AuditLog;
 import com.bumsoap.notes.models.Note;
 import com.bumsoap.notes.service.NoteAudit;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class NoteAuditImpl implements NoteAudit {
   @Override
   public void logCreate(String username, Note note) {
+    var noteLog = new AuditLog();
+    noteLog.setNoteId(note.getId());
+    noteLog.setNoteContent(note.getContent());
+    noteLog.setUsername(username);
+    noteLog.setAction("create");
+    noteLog.setTimestamp(LocalDateTime.now());
+
   }
 
   @Override
