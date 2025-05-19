329fc4474adaaae93a72983783e9b1df1fce2b6d
박종범 <jbpark03@gmail.com>
2025-05-19 09:48:28 +0900

    allow/secure audit endpoints only for admin

--- src/main/java/com/bumsoap/notes/cont/NoteAuditLogCon.java
+++ src/main/java/com/bumsoap/notes/cont/NoteAuditLogCon.java
@@ -3,6 +3,7 @@ package com.bumsoap.notes.cont;
 import com.bumsoap.notes.models.AuditLog;
 import com.bumsoap.notes.service.NoteAudit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,13 @@ public class NoteAuditLogCon {
   private NoteAudit noteAudit;
 
   @GetMapping
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   public List<AuditLog> getAllAuditLogs() {
     return noteAudit.getAllAuditLogs();
   }
 
   @GetMapping("/note/{noteId}")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   public List<AuditLog> getAuditLogsFor(@PathVariable Long noteId) {
     return noteAudit.getAuditLogsFor(noteId);
   }
