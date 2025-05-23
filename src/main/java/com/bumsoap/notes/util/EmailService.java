102d62513d845265942c4ae524d0c556431e3dcf
박종범 <jbpark03@gmail.com>
2025-05-23 16:27:48 +0900

    email sender bean class

---
+++ src/main/java/com/bumsoap/notes/util/EmailService.java
@@ -0,0 +1,20 @@
+package com.bumsoap.notes.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+  @Autowired
+  private JavaMailSender mailSender;
+
+  public void sendPasswordResetMail(String toEmail, String resetUrl) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setTo(toEmail);
+    message.setSubject("패스워드 재 설정 링크");
+    message.setText("패스워드 재 설정 링크: " + resetUrl);
+    mailSender.send(message);
+  }
+}
