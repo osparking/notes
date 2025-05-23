562c1120cc4aa3c02cc658c2db741a7f8f94a1d7
박종범 <jbpark03@gmail.com>
2025-05-23 11:48:50 +0900

    password reset token constructor

--- src/main/java/com/bumsoap/notes/models/PasswordResetToken.java
+++ src/main/java/com/bumsoap/notes/models/PasswordResetToken.java
@@ -23,4 +23,11 @@ public class PasswordResetToken {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
+
+  public PasswordResetToken(String token,
+                            Instant expiryDate, User user) {
+    this.token = token;
+    this.expiryDate = expiryDate;
+    this.user = user;
+  }
 }
