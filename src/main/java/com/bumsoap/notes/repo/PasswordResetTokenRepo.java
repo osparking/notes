69693b7888f2e0110acce1b29d599f5165fe0751
박종범 <jbpark03@gmail.com>
2025-05-25 16:29:30 +0900

    covers when password reset token not found

--- src/main/java/com/bumsoap/notes/repo/PasswordResetTokenRepo.java
+++ src/main/java/com/bumsoap/notes/repo/PasswordResetTokenRepo.java
@@ -3,7 +3,9 @@ package com.bumsoap.notes.repo;
 import com.bumsoap.notes.models.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PasswordResetTokenRepo
     extends JpaRepository<PasswordResetToken, Long> {
-  PasswordResetToken findByToken(String token);
+  Optional<PasswordResetToken> findByToken(String token);
 }
