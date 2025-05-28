6bda350406ccfc894fa5b4ffd25a2ed228bb3d29
박종범 <jbpark03@gmail.com>
2025-05-28 11:29:52 +0900

    skeleton and essential method - onauthenticationsuccess

---
+++ src/main/java/com/bumsoap/notes/config/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.bumsoap.notes.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginSuccessHandler
+    extends SavedRequestAwareAuthenticationSuccessHandler {
+
+  @Override
+  public void onAuthenticationSuccess(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      Authentication authentication)
+      throws ServletException, IOException
+  {}
+
+}
