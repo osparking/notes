c8fbc356aa58122d96119fada660f1ad454dcc67
박종범 <jbpark03@gmail.com>
2025-05-28 21:21:38 +0900

    prints class name of all beans in context

--- src/main/java/com/bumsoap/notes/NotesApplication.java
+++ src/main/java/com/bumsoap/notes/NotesApplication.java
@@ -1,12 +1,31 @@
 package com.bumsoap.notes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class NotesApplication {
+public class NotesApplication implements CommandLineRunner {
+
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		String[] beanNames = applicationContext.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			Object bean = applicationContext.getBean(beanName);
+			System.out.println(
+					"빈이름" + beanName + " : " + bean.getClass().getName());
+		}
 	}
 }
