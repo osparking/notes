2d599d28e57a179986b6bcfeb4e96d78ae8ee8f4
박종범 <jbpark03@gmail.com>
2025-05-28 11:31:59 +0900

    data members for oauth2 login success handler

--- src/main/java/com/bumsoap/notes/config/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/bumsoap/notes/config/OAuth2LoginSuccessHandler.java
@@ -1,9 +1,14 @@
 package com.bumsoap.notes.config;
 
+import com.bumsoap.notes.repo.RoleRepo;
+import com.bumsoap.notes.security.jwt.JwtUtils;
+import com.bumsoap.notes.service.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -14,6 +19,21 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler
     extends SavedRequestAwareAuthenticationSuccessHandler {
+
+  @Autowired
+  private final UserService userService;
+
+  @Autowired
+  private final JwtUtils jwtUtils;
+
+  @Autowired
+  RoleRepo roleRepository;
+
+  @Value("${frontend.url}")
+  private String frontendUrl;
+
+  String username;
+  String idAttributeKey;
 
   @Override
   public void onAuthenticationSuccess(
