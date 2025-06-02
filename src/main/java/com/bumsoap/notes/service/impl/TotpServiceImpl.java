41cee6bee770880fa63035d005f6b811fba3b10a
박종범 <jbpark03@gmail.com>
2025-06-02 11:53:01 +0900

    generates authenticator key object

--- src/main/java/com/bumsoap/notes/service/impl/TotpServiceImpl.java
+++ src/main/java/com/bumsoap/notes/service/impl/TotpServiceImpl.java
@@ -2,11 +2,16 @@ package com.bumsoap.notes.service.impl;
 
 import com.bumsoap.notes.service.TotpService;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 
 public class TotpServiceImpl implements TotpService {
   private final GoogleAuthenticator gAuthetor;
 
   public TotpServiceImpl(GoogleAuthenticator googleAuthenticator) {
     this.gAuthetor = googleAuthenticator;
+  }
+
+  public GoogleAuthenticatorKey generateSecret() {
+    return gAuthetor.createCredentials();
   }
 }
