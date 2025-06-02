36bcbc42dabbe9cdc6d23d370f5beeb91028d0fe
박종범 <jbpark03@gmail.com>
2025-06-02 12:14:01 +0900

    calling URL will generate QR code

--- src/main/java/com/bumsoap/notes/service/impl/TotpServiceImpl.java
+++ src/main/java/com/bumsoap/notes/service/impl/TotpServiceImpl.java
@@ -3,6 +3,9 @@ package com.bumsoap.notes.service.impl;
 import com.bumsoap.notes.service.TotpService;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+
+import java.nio.channels.spi.AbstractSelectionKey;
 
 public class TotpServiceImpl implements TotpService {
   private final GoogleAuthenticator gAuthetor;
@@ -13,5 +16,11 @@ public class TotpServiceImpl implements TotpService {
 
   public GoogleAuthenticatorKey generateSecret() {
     return gAuthetor.createCredentials();
+  }
+
+  public String getQRcodeUrl(
+      GoogleAuthenticatorKey secret, String username) {
+    return GoogleAuthenticatorQRGenerator.getOtpAuthURL(
+        "비밀 노트 응용", username, secret);
   }
 }
