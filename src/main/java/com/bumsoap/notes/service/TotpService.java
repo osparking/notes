70cfb5c86ccbc5591080f5969e349fff93d78d26
박종범 <jbpark03@gmail.com>
2025-06-02 13:36:15 +0900

    declare service bean, pull methods

--- src/main/java/com/bumsoap/notes/service/TotpService.java
+++ src/main/java/com/bumsoap/notes/service/TotpService.java
@@ -1,4 +1,12 @@
 package com.bumsoap.notes.service;
 
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+
 public interface TotpService {
+  GoogleAuthenticatorKey generateSecret();
+
+  String getQRcodeUrl(
+      GoogleAuthenticatorKey secret, String username);
+
+  boolean verifyCode(String secret, int code);
 }
--- src/main/java/com/bumsoap/notes/service/impl/TotpServiceImpl.java
+++ src/main/java/com/bumsoap/notes/service/impl/TotpServiceImpl.java
@@ -4,9 +4,9 @@ import com.bumsoap.notes.service.TotpService;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import org.springframework.stereotype.Service;
 
-import java.nio.channels.spi.AbstractSelectionKey;
-
+@Service
 public class TotpServiceImpl implements TotpService {
   private final GoogleAuthenticator gAuthetor;
 
@@ -18,16 +18,19 @@ public class TotpServiceImpl implements TotpService {
     this.gAuthetor = new GoogleAuthenticator();
   }
 
+  @Override
   public GoogleAuthenticatorKey generateSecret() {
     return gAuthetor.createCredentials();
   }
 
+  @Override
   public String getQRcodeUrl(
       GoogleAuthenticatorKey secret, String username) {
     return GoogleAuthenticatorQRGenerator.getOtpAuthURL(
         "비밀 노트 응용", username, secret);
   }
 
+  @Override
   public boolean verifyCode(String secret, int code) {
     return gAuthetor.authorize(secret, code);
   }
